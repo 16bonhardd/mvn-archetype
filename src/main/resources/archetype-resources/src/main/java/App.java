55691b09a31cc90c3d28c974a87151c0eda0faa2
16bonhardd <16bonhardd@gmail.com>
2023-07-18 21:02:25 -0500

    updated configs for hello world setup

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,9 @@
+package ${groupId};
+
+@Slf4j
+public class App {
+
+    public static void main(String[] args){
+        log.info("hello world");
+    }
+}
---
+++ src/main/resources/archetype-resources/src/test/java/AppTest.java
@@ -0,0 +1,13 @@
+package ${groupId};
+
+import ${groupId}.App;
+
+
+public class AppTest {
+
+    @Test
+    void testMain(){
+        App app = new App();
+        app.main();
+    }
+}
